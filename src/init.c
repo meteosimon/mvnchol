7b508bf2a55ddf5a75cfbfc7ae1d2cdd3f34e04f
Thorsten Simon <Thorsten.Simon@uibk.ac.at>
2019-11-08 16:06:36 +0100

    Init variables

---
+++ src/init.c
@@ -0,0 +1,20 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+extern SEXP spellsC(SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"log_dmvnchol", (DL_FUNC) &log_dmvnchol, 7},
+    {NULL, NULL, 0}
+};
+
+void R_init_bamlssMVN(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
+
+
---
+++ src/mvnchol.c
@@ -0,0 +1,65 @@
+#include <R.h>
+#include <Rmath.h>
+#include <Rdefines.h>
+#include <Rinternals.h>
+
+/*
+Function: log_dmvnchol
+
+Input:
+Y observations (matrix)
+PAR parameters (matrix)
+N sample size (integer)
+K dimension of multivariate distribution
+MJ column indices for location parameters
+DJ column indices for diagonal elements of L^{-1}
+TJ column indices for element in lower triangle of L^{-1}
+*/
+SEXP log_dmvnchol(SEXP Y, SEXP PAR, SEXP N, SEXP K, SEXP MJ, SEXP DJ, SEXP TJ)
+{
+/* Pointers for obs and par */
+  double *Yptr = REAL(Y);
+  double *PARptr = REAL(PAR);
+
+/* integers from input */
+  int n = asInteger(N);
+  int k = asInteger(K);
+  int mj = asInteger(MJ);
+  int dj = asInteger(DJ);
+  int tj = asInteger(TJ);
+
+/* loop counters */
+  int i, j;
+
+/* inverse of L matrix for single obs */
+  SEXP Linv;
+  PROTECT(allocMatrix(REALSXP, k, k);
+  double *Linvptr = REAL(Linv);
+
+/* residuals for single obs */
+  SEXP ymu;
+  PROTECT(ymu = allocVector(REALSXP, k));
+  double *ymuptr = REAL(ymu);
+
+/* return value: vector of log-densities */
+  SEXP d;
+  PROTECT(d = allocVector(REALSXP, n));
+  double *dptr = REAL(d);
+
+/* initialize three terms of log-density */
+  double lpi = -0.5 * (float)k * log(2.0 * 3.141592653589793115998);
+  double det = 0.0;
+  double nrm = 0.0;
+
+/* */
+  for(i = 1; i < n; i++) {
+    dptr[i] = lpi;
+  }
+
+/* return */
+  UNPROTECT(3);
+  return d;
+}
+
+
+
