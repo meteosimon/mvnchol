c4cbcc0bb396f4d02b1ac8e96d0bde3f49b05c79
Thorsten Simon <Thorsten.Simon@uibk.ac.at>
2019-11-08 22:11:14 +0100

    Struggle with documenting data sets

---
+++ R/datasets.R
@@ -0,0 +1,12 @@
+#' Reference family.
+#'
+#' A BAMLSS family for a multivariate Gaussian using Cholesky decomposition
+#' that implemnts the log-density and scores in R (correctly).
+#' (A bamlss family object.)
+"reffam"
+
+#' Reference data.
+#'
+#' Simulated data to test the implementation of the bamlss family.
+"simdata"
+
