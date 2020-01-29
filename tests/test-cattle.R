726cd8155f1fbc7972538179f4eb481a8fbc05d9
Thomas Muschinski <thomas.muschinski@uibk.ac.at>
2020-01-29 17:06:33 +0100

    Finished plotting of log-innovation variances and autoregressive coefs for regul and unregul models

--- tests/test-cattle.R
+++ tests/test-cattle.R
@@ -9,16 +9,6 @@ rownames(df) <- df$id
 df$id <- NULL
 
 
-# source("../R/prototype_chol.R")
-
-### C routines not working...
-# lambda_score_mvnchol <- lambda_score_mvnchol_ref
-# lamdiag_score_mvnchol <- lamdiag_score_mvnchol_ref
-# mu_score_mvnchol <- mu_score_mvnchol_ref
-# log_dmvnchol <- log_dmvnchol_ref
-
-
-
 f_mus <- paste0(paste0(names(df)[2:12], " ~ 0 + s(group, bs = 're')"))
 f_lamdiags <- paste0(paste0("lamdiag", seq_len(11)), " ~ 0 + s(group, bs = 're')")
 f_lambdas <- paste0(combn(seq_len(11), 2, 
@@ -81,7 +71,47 @@ matricize <- function(p) {
 ## Helper function to extract offdiagonals of matrix as vector
 offdiag <- function(x, k = 0) {x[which((col(x) - row(x)) == k)]}
 
-## Plot log()
+## Reproduce Fig. 2 in jmcm package JSS paper
+## (log innov variances vs. time) and (autoreg coefficients vs. lag)
+
+# Use model estimated without penalization
+b1 <- readRDS("cattle_model1.RDS")
+p1 <- predict(b1, type = "parameter")
+
+p1_matrices <- matricize(p1)
+T_A1 <- p1_matrices$T[[1]]
+loginnovars1 <- log(1/diag(p1_matrices$D[[1]])^2)
+
+genautoregs1 <- matrix(NA, nrow = 10, ncol = 10) 
+for (i in 1:10) {
+  genautoregs1[1:length(offdiag(T_A1, i)), i] <- -offdiag(T_A1, i)    
+}
+
+# Use model with ridge penalization
+b2 <- readRDS("cattle_model2.RDS")
+p2 <- predict(b2, type = "parameter")
+
+p2_matrices <- matricize(p2)
+T_A2 <- p2_matrices$T[[1]]
+loginnovars2 <- log(1/diag(p2_matrices$D[[1]])^2)
+
+genautoregs2 <- matrix(NA, nrow = 10, ncol = 10) 
+for (i in 1:10) {
+  genautoregs2[1:length(offdiag(T_A2, i)), i] <- -offdiag(T_A2, i)    
+}
+
+
+xx <- matrix(1:10, ncol = 10, nrow = 10, byrow = TRUE)
+
+
+x11()
+par(mfrow = c(2, 2))
+plot(loginnovars1, xlab = "Time", ylab = "Log-innovat. var.")
+plot(xx, genautoregs1, xlab = "Lag", ylab = "Autoregres. coeffic.")
+plot(loginnovars2, xlab = "Time", ylab = "Log-innovat. var.")
+plot(xx, genautoregs2, xlab = "Lag", ylab = "Autoregres. coeffic.")
+
+
 
 
 
