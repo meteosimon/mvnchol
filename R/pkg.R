632628c06da71c601a7af335d37799367210bf05
Thorsten Simon <Thorsten.Simon@uibk.ac.at>
2020-12-22 16:12:13 +0100

    Document pkg

---
+++ R/pkg.R
@@ -0,0 +1,12 @@
+#' @details
+#' This is the package bamlss_mvn.
+#' It implements bamlss families for modelling multivariate
+#' responses. The variance-covariance matrix is re-parameterized by
+#' the (modified) Cholesky decomposition.
+#' The key function to generate a family is \code{mvnchol_bamlss}.
+#' You can adjust the dimension \code{k} of the multivariate normal,
+#' and its \cdoe{type} of Cholesky decompositions: \code{"basic"} or
+#' \code{"modified"}.
+#' @keywords internal
+"_PACKAGE"
+
