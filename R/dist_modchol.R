36b2118b36f818f3543e3c92006e1d692eb2a7a1
Thorsten Simon <Thorsten.Simon@uibk.ac.at>
2021-11-09 15:51:13 +0100

    Skeleton for disttree famliy (modified)

--- R/dist_modchol.R
+++ R/dist_modchol.R
@@ -1,8 +1,18 @@
 ## distree family for modified Choesky
+## compare with dist_mvnorn l. 2208 in families.R of disttree pkg
 dist_mvn_modchol <- function(k, ...) {
     stop("distree family for modified Choesky is not implemented yet!")
+    klt <- k * (k - 1L) / 2L  ## number of elements in the lower triangle of Sigma (and similar matrices)
+    kn <- k + k + klt ## number of all distributional parameters
+    nms_par <- c("")  ## character vector with names of distributional paramters
+    nms_eta <- c("")  ## character vector with names of predictors, ie link(distributional paramters)
 
     ## TODO: density
+    ## Input:
+    ## y ...... observed samples (matrix)
+    ## eta .... named numeric vector (one entry for each distributional parameter)
+    ## Output:
+    ## numeric vector with contrbutions to density 
     ddist <-  function(y, eta, log = TRUE, weights = NULL, sum = FALSE) {
 
     }
@@ -13,23 +23,26 @@ dist_mvn_modchol <- function(k, ...) {
     }
 
     ## links TODO: must be conditioned on k
-    link <- c("identity", "log")
+    link <- c("identity", "log")[c(rep(1, k), rep(2, k), rep(1, klt))]
 
     linkfun <- function(par) {
-        eta <- c(par[1], log(par[2]))
-        names(eta) <- c("mu", "log(sigma)")
+        eta <- par
+        eta[seq_len(k) + k] <- log(par[seq_len(k) + k])
+        names(eta) <- nms_eta
         return(eta)
     }
 
     linkinv <- function(eta) {
-        par <- c(eta[1], exp(eta[2]))
-        names(par) <- c("mu", "sigma")
+        par <- eta
+        par[seq_len(k) + k] <- exp(eta[seq_len(k) + k])
+        names(par) <- nms_par
         return(par)
     }
     
     linkinvdr <- function(eta) {
-        dpardeta <- c(1, exp(eta[2]))
-        names(dpardeta) <- c("mu", "sigma")
+        dpardeta <- rep(1, kn)
+        dpardeta[seq_len(k) + k] <- exp(eta[seq_len(k) + k])
+        names(dpardeta) <- nms_par
         return(dpardeta)
     }
 
